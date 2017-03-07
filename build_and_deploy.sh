62de078c3bb52fed25223094c25512f8f2f51942
Josh Long <josh@joshlong.com>
2017-03-07 03:15:12 -0800

    build and deploy

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -7,28 +7,38 @@ function app_domain(){
 
 rd=$( cd `dirname $0` && pwd )
 app=s3-broker
+broker=amazon-s3
 service_brokers=$broker
 db=s3-broker-db
-broker=amazon-s3
 plan=s3-basic
 
 
+# reset
+function reset(){
+  cf delete-service-broker -f $broker
+  cf d -f $app
+  cf ds -f $db
+  cf delete-orphaned-routes
+}
+
+reset
+
+# create
 cf d -f $app
 cf delete-orphaned-routes -f
 
-cf s | grep $db || cf cs $db p-mysql 100mb
+cf s | grep $db || cf cs p-mysql 512mb $db
 cd $rd/s3-service-broker
 
 path_to_jar=$rd/s3-service-broker/target/s3-service-broker.jar
 
+ls -la $rd/s3-service-broker/manifest.yml 
 [ -e $path_to_jar ] && echo "jar ${path_to_jar} already exists. Not rebuilding." || mvn -DskipTests=true clean install
-
 cf a | grep $app || cf push -f $rd/s3-service-broker/manifest.yml
 
-app_url=http://`app_domain $app`
-echo $app_url
+app_url=`app_domain $app`
+app_url=https://${app_url}
 
 cf create-service-broker $broker admin admin $app_url
-
 cf enable-service-access $service_brokers
 cf create-service $service_brokers $plan my-s3-sample-service
