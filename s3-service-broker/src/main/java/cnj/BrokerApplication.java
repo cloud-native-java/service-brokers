3d7057db5de3cc67b57d9970fd5b2ff2112b56bf
Josh Long <josh@joshlong.com>
2017-03-19 19:52:10 -0700

    service brokers ftw

--- s3-service-broker/src/main/java/cnj/BrokerApplication.java
+++ s3-service-broker/src/main/java/cnj/BrokerApplication.java
@@ -1,10 +1,19 @@
 package cnj;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BrokerApplication {
+
+ @Autowired
+ public void configure (Environment e){
+  System.out.println(Arrays.toString(e.getActiveProfiles()));
+ }
 
  public static void main(String[] args) {
   SpringApplication.run(BrokerApplication.class, args);
