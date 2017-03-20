2db97be5481227c872bcba2a5f41ae0e90232578
Josh Long <josh@joshlong.com>
2017-03-20 00:53:13 -0700

    broker

--- s3-service-broker/src/main/java/cnj/BrokerApplication.java
+++ s3-service-broker/src/main/java/cnj/BrokerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrokerApplication {
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
   SpringApplication.run(BrokerApplication.class, args);
  }
 }
